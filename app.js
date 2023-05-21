a034204ae04a7cef1257424a45206187be052857
IrisFrancisco <124634272+IrisFrancisco@users.noreply.github.com>
2023-05-21 18:05:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const weddingDate = new Date('2023-08-26T11:30:00');
-const daysCounter = document.querySelector('#days');
-const hoursCounter = document.querySelector('#hours');
-const minutesCounter = document.querySelector('#minutes');
-const secondsCounter = document.querySelector('#seconds');
+const weddingDate = new Date("2023-08-26T11:30:00");
+const daysCounter = document.querySelector("#days");
+const hoursCounter = document.querySelector("#hours");
+const minutesCounter = document.querySelector("#minutes");
+const secondsCounter = document.querySelector("#seconds");
 
-function countdownTimer(){
-    let currDate = new Date()
+function countdownTimer() {
+  let currDate = new Date();
 
-    // get total seconds between the times
-    let delta = Math.abs(weddingDate - currDate) / 1000;
+  // get total seconds between the times
+  let delta = Math.abs(weddingDate - currDate) / 1000;
 
-    // calculate (and subtract) whole days
-    let days = Math.floor(delta / 86400);
-    delta -= days * 86400;
+  // calculate (and subtract) whole days
+  let days = Math.floor(delta / 86400);
+  delta -= days * 86400;
 
-    // calculate (and subtract) whole hours
-    let hours = Math.floor(delta / 3600) % 24;
-    delta -= hours * 3600;
+  // calculate (and subtract) whole hours
+  let hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
 
-    // calculate (and subtract) whole minutes
-    let minutes = Math.floor(delta / 60) % 60;
-    delta -= minutes * 60;
+  // calculate (and subtract) whole minutes
+  let minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
 
-    // what's left is seconds
-    let seconds = Math.floor(delta % 60);  // in theory the modulus is not required
+  // what's left is seconds
+  let seconds = Math.floor(delta % 60); // in theory the modulus is not required
 
-    //console.log(days, hours, minutes, seconds)
-    daysCounter.innerText=days
-    hoursCounter.innerText=hours
-    minutesCounter.innerText=minutes
-    secondsCounter.innerText=seconds
+  //console.log(days, hours, minutes, seconds)
+  daysCounter.innerText = days;
+  hoursCounter.innerText = hours;
+  minutesCounter.innerText = minutes;
+  secondsCounter.innerText = seconds;
 }
 
 countdownTimer();
 setInterval(countdownTimer, 1000);
-
 
 //const currDate = new Date();
 
@@ -49,5 +48,16 @@ setInterval(countdownTimer, 1000);
 //let seconds = Math.abs(weddingDate - auxDate) / 1000;
 //console.log(daysToWedding, hours, minutes, seconds);
 
+let isShowIban = false;
 
-
+function handleViewIban() {
+  if (!isShowIban) {
+    const text = "PT50 0023 0000 45685404933 94";
+    var paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    paragraph.classList.add("text-center", "m-3");
+    var element = document.getElementById("gifts");
+    element.appendChild(paragraph);
+    isShowIban = true;
+  }
+}
